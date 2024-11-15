4767b94cb06ff1693750c64c2931f0b59faf5af8
LuongBinh0901 <nguyenbinh2366@gmil.com>
2024-11-15 16:24:02 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Import các thư viện
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Tạo một mảng x từ 0 đến 2π với bước nhảy nhỏ
+#x = np.linspace(0, 2 * np.pi, 100)  # 100 điểm từ 0 đến 2π
+#y = np.sin(x)  # Hàm y = sin(x)
+
+# Vẽ đồ thị
+#plt.plot(x, y, label="y = sin(x)")  # Vẽ đường y = sin(x)
+#plt.xlabel("x")                     # Nhãn trục x
+#plt.ylabel("y")                     # Nhãn trục y
+#plt.title("Đồ thị của hàm số y = sin(x)")  # Tiêu đề
+#plt.legend()                         # Hiển thị chú thích
+#plt.grid(True)                       # Hiển thị lưới
+#plt.show()                           # Hiển thị đồ thị
+import numpy as np
+arr = np.zeros((2, 4))  # Tạo mảng 2x4 với tất cả giá trị bằng 0
+print(arr)
+
